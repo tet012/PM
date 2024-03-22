faf26c4003ae316c1fdb50dabd7f8e392bf11ee2
tet012 <120034406+tet012@users.noreply.github.com>
2024-03-22 17:23:52 +0100

    RANDO%MMMMMMMMMMMMMMMMM

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 export const params = {
+    // EVOLUTION
+    entropy: 0,
+
     //PETALS
     petals: 8,
-    width: 25,
+    width: 250,
     height: 150,
-    petalX: 0,
-    petalY: 50,
-
-    //SHAPE
-    x1: 0,
-    x2: 0,
-    y1: 0,
-    y2: 100,
-    z1: 150,
-    z2: 0,
+    petalX: 50,
+    petalY: -50,
 
     //RADIUS
-    radius_A: 0,
-    radius_B: 0,
-    radius_C: 0,
-    radius_D: 0,
+    radius_x1: 100,
+    radius_y1: 200,
+    radius_x2: 0,
+    radius_y2: 0,
+    radius_x3: 100,
+    radius_y3: 200,
+    radius_x4: 0,
+    radius_y4: 0,
 
     //LAYER
-    layers: 16,
+    layers: 1,
     offsetX: 0,
     offsetY: 0,
     scale: 0.1,
@@ -32,14 +31,29 @@ export const params = {
 
     //COLOR
     fill: true,
-    gradientType: 'radial',
-    position: { x: 250, y: 50 },
-    gradient_direction: 'down',
+    position: { x: 100, y: 50 },
     color_shift: 10,
-    hue_1: 180,
-    hue_2: 240,
-    opacity_1: 0.9,
-    opacity_2: 0.9,
+    hue_1: 90,
+    hue_2: 360,
+    // opacity_1: 0.25,
+    // opacity_2: 0.25,
+    opacity_1: 0.7,
+    opacity_2: 0.7,
+    light_1: 75,
+    light_2: 75,
+    sat_1: 100,
+    sat_2: 50,
+
+    //Custom Shape
+    enable: true,
+    x1: 0,
+    y1: 0,
+    x2: 0,
+    y2: 100,
+    x3: 150,
+    y3: 0,
+    x4: 150,
+    y4: 0,
 
     //STROKE
     strokeWidth: 0,
@@ -47,12 +61,7 @@ export const params = {
 
     //FX
     blur: 0,
-    shadow: 0,
-
-    //PATTERN
-    selectedClass: 'bg_pattern_1',
-    patternColor1: '#e5e5f7',
-    patternColor2: '#444cf7',
+    shadow: 10,
 };
 
 export function generatePetals() {
@@ -60,83 +69,143 @@ export function generatePetals() {
     container.style.position = 'relative';
     container.innerHTML = '';
 
-    for (let j = 1; j <= params.layers; j++) {
-        const hue_1 = params.hue_1 + (j - 1) * params.color_shift;
-        const hue_2 = params.hue_2 + (j - 1) * params.color_shift;
+    const a1 = Math.random() * 100;
+    const b1 = Math.random() * 100;
+    const a2 = Math.random() * 100;
+    const b2 = Math.random() * 100;
+    const a3 = Math.random() * 100;
+    const b3 = Math.random() * 100;
+    const a4 = Math.random() * 100;
+    const b4 = Math.random() * 100;
 
+    const r_a1 = Math.random() * 200;
+    const r_b1 = Math.random() * 200;
+    const r_a2 = Math.random() * 200;
+    const r_b2 = Math.random() * 200;
+    const r_a3 = Math.random() * 200;
+    const r_b3 = Math.random() * 200;
+    const r_a4 = Math.random() * 200;
+    const r_b4 = Math.random() * 200;
+
+    const layersNumbr = Math.floor(Math.random() * 32);
+    const petalsNumber = Math.max(4, Math.floor(Math.random() * (16)));
+
+    const baseHue_1 = Math.random() * 360;
+    const baseHue_2 = Math.random() * 360;
+    const baseShift = Math.random() * 50
+
+    const baseScale = Math.random() * 16;
+    const baseWidth = Math.floor(Math.random() * 500) + 100;
+    const baseHeight = Math.floor(Math.random() * 500) + 100;
+
+    const angle = Math.floor(Math.random() * 361) - 180;
+    const op_1 = Math.random();
+    const op_2 = Math.random();
+
+    const px = Math.floor(Math.random() * 201) - 100;
+    const py = Math.floor(Math.random() * 201) - 100;
+
+    for (let j = 1; j <= layersNumbr; j++) {
         const group = document.createElement('div');
         group.className = 'group';
-        group.style.position = 'absolute';
+        const hue_1 = baseHue_1 + baseShift * (j - 1);
+        const hue_2 = baseHue_2 + baseShift * (j - 1);
+
         const groupRotation = params.rotation * (j - 1);
         group.style.transform = `translate(${params.offsetX * (j - 1)}px, ${params.offsetY * (j - 1)}px) rotate(${groupRotation}deg)`;
 
-        for (let i = 0; i < params.petals; i++) {
-            const scale = 1 + (j - 1) * params.scale;
-            const width = params.width * scale;
-            const height = params.height * scale;
+        for (let i = 0; i < petalsNumber; i++) {
             const petal = document.createElement('div');
+            const petal_gradient = document.createElement('div');
+            const petal_inner_shadow = document.createElement('div');
 
-            // SHAPE
-            // petal.className = `petal ${params.selectedClass}`;
+            const scale = 1 + (j - 1) * baseScale;
+            const width = baseWidth + scale;
+            const height = baseHeight + scale;
+
+            const i_width = width * 2;
+            const i_height = height * 2;
+
             petal.classList.add('petal');
+            petal_gradient.classList.add('petal-gradient');
+            petal_inner_shadow.classList.add('petal-inner_shadow');
 
             petal.style.width = `${width}px`;
             petal.style.height = `${height}px`;
             petal.style.marginLeft = `-${width / 2}px`;
             petal.style.marginTop = `-${height / 2}px`;
-            petal.style.borderRadius = `${params.radius_A}% ${params.radius_B}% ${params.radius_C}% ${params.radius_D}%`;
+            petal.style.borderRadius =
+                `${r_a1}% ${r_b1}% ${r_a2}% ${r_b2}% / 
+            ${r_a3}% ${r_b3}% ${r_a4}% ${r_b4}%`;
 
-            // FILL & BORDER
-            let direction;
-            switch (params.gradient_direction) {
-                case 'down':
-                    direction = 'to bottom';
-                    break;
-                case 'up':
-                    direction = 'to top';
-                    break;
-                case 'left':
-                    direction = 'to left';
-                    break;
-                case 'right':
-                    direction = 'to right';
-                    break;
-                default:
-                    direction = 'to right';
-            }
+            petal_gradient.style.width = `${i_width}px`;
+            petal_gradient.style.height = `${i_height}px`;
 
-            const updateStroke = params.strokeWidth + 1
+            const rotationPerPetal = (360 / petalsNumber) * i + angle * j;
+
             if (params.fill) {
-                let background;
-                if (params.gradientType === 'linear') {
-                    background = `linear-gradient(${direction}, hsl(${hue_1}, 100%, 50%, ${params.opacity_1}), hsl(${hue_2}, 100%, 50%, ${params.opacity_2}))`;
-                } else if (params.gradientType === 'radial') {
-                    background = `radial-gradient(circle at ${params.position.x}px ${params.position.y}px, hsl(${hue_1}, 100%, 50%, ${params.opacity_1}), hsl(${hue_2}, 100%, 50%, ${params.opacity_2}))`;
-                }
+                const background = `hsla(${hue_1}, 100%, 50%, ${params.opacity_1})`
                 petal.style.background = background;
-                petal.style.border = `${params.strokeWidth}px ${params.strokeStyle} hsl(${hue_1}, 100%, 50%)`;
-            } else {
-                petal.style.border = `${params.strokeWidth + 1}px ${params.strokeStyle} hsl(${hue_1}, 100%, 50%)`;
+                petal.style.border = 'none'
+
+                const innerBackground = `radial-gradient(
+                    hsla(${hue_1}, ${params.light_1}%, ${params.sat_1}%, ${op_1}), 
+                    hsla(${hue_2}, ${params.light_2}%,  ${params.sat_2}%, ${op_2}))`;
+                // const innerBackground = `repeating-radial-gradient(circle, black, black 5px, white 5px, white 10px)`;
+                const innerBackground2 = `
+                radial-gradient(circle at 50% 50%, transparent 20%, #00D4F0 21%, #00D4F0 34%, transparent 35%, transparent),
+                radial-gradient(circle at 0% 50%, transparent 20%, #00D4F0 21%, #00D4F0 34%, transparent 35%, transparent)`;
+                petal_gradient.style.background = innerBackground;
+                // petal_gradient.style.backgroundRepeat = 'no-repeat'; // Prevent repeating the gradient tiles
+                // petal_gradient.style.backgroundPosition = `right top, left bottom`; // Example positions
+                // petal_gradient.style.backgroundSize = `auto, 100%`; // Adjust size as needed
+
+                // petal.style.backdropFilter = 'blur(5px)';
+                // petal_gradient.style.filter = `backdrop( blur(80px) )`;
+                petal_gradient.style.backgroundPosition = `${params.position.x}px ${params.position.y}px`;
+                // petal_gradient.style.transform = `rotate(${reverseAngle}deg)`;
             }
 
-            petal.style.clipPath = `polygon(${params.x1}% ${params.x2}%, ${params.y1}%  ${params.y2}%,  ${params.z1}%  ${params.z2}%)`;
+            const blur = (j - 1) * params.blur;
+            const shadowBlur = params.shadow;
 
-            // EFFECTS
-            let blur = (j - 1) * params.blur;
-            let shadow = params.shadow * (j - 1)
-            petal.style.boxShadow = `0 0 ${shadow}px hsl(${hue_1}, 100%, 50%)`;
+            petal_inner_shadow.style.width = `${width}px`;
+            petal_inner_shadow.style.height = `${height}px`;
+            petal_inner_shadow.style.position = 'absolute';
+            petal_inner_shadow.style.zIndex = '100';
+            // petal_inner_shadow.style.borderRadius = `${params.radius_A}% ${params.radius_B}% ${params.radius_C}% ${params.radius_D}%`;
+            petal_inner_shadow.style.boxShadow = `
+            inset 0px 0px 128px hsla(0, 0%, 0%, 0.1),
+            inset 0px 0px 64px hsla(0, 0%, 0%, 0.3),
+            inset 0px 0px 32px -8px hsla(0, 0%, 0%, 0.5),
+            inset 0px 0px 8px -4px hsla(0, 0%, 0%, 1)`;
+
+            // petal_inner_shadow.style.boxShadow = `
+            // 0px 0px 128px -32px hsla(0, 0%, 0%, 0.1), 
+            // inset -16px 16px 64px -24px hsla(0, 0%, 0%, 0.5)`,
+
             petal.style.filter = `blur(${blur}px)`;
+            petal.style.transform = `rotate(${rotationPerPetal}deg) translate(${px}%, ${py}%)`;
+            petal.style.clipPath = `polygon(${a1}% ${b1}%, ${a2}% ${b2}%, ${a3}% ${b3}%, ${a4}% ${b4}%)`;
 
-            const rotationPerPetal = (360 / params.petals) * i;
-            petal.style.transform = `rotate(${rotationPerPetal}deg) translate(${params.petalX}%, ${params.petalY}%) rotate(${params.angle}deg)`;
+            // petal_gradient.style.transform = `rotate(${rotationPerPetal}deg) `;
 
+            petal.appendChild(petal_gradient);
+            petal.appendChild(petal_inner_shadow);
             group.appendChild(petal);
         }
-
         container.insertBefore(group, container.firstChild);
     }
+
+    const heart = document.createElement('div');
+    heart.className = 'heart';
+    heart.style.width = '150px';
+    heart.style.height = '150px';
+    heart.style.background = `hsla(${baseHue_1}, 100%, 50%, 1)`;
+    heart.style.borderRadius = '50%';
+    heart.style.position = 'absolute';
+    heart.style.boxShadow = '0px 0px 32px -8px black, inset 0px 0px 64px -16px hsla(0, 0%, 0%, 0.25), inset 0px 0px 32px -8px hsla(0, 0%, 0%, 0.5) '
+    container.appendChild(heart);
 }
 
 generatePetals();
-
-
--- ui.js
+++ ui.js
@@ -1,94 +1,62 @@
 import { Pane } from 'https://cdn.jsdelivr.net/npm/tweakpane@4.0.3/dist/tweakpane.min.js';
 import { params, generatePetals } from './script.js';
-const pane = new Pane({ title: 'Petal Generator' });
+import * as TweakpaneEssentialsPlugin from './tweakpane-plugin-camerakit-0.3.0.min.js';
+
+const pane = new Pane({ title: 'Parameters' });
+pane.registerPlugin(TweakpaneEssentialsPlugin);
+
 
 // PETALS
-const petals = pane.addFolder({ title: 'Petals' });
-petals.addBinding(params, 'petals', { min: 1, max: 32, step: 1 }).on('change', generatePetals);
+const petals = pane.addFolder({
+    title: 'Petals', expanded: false,
+});
+petals.addBinding(params, 'petals', { min: 1, max: 32, step: 1, label: 'Number' },).on('change', generatePetals);
 petals.addBlade({
     view: 'separator',
 });
-petals.addBinding(params, 'width', { min: 0, max: 800, step: 0.1 }).on('change', generatePetals);
-petals.addBinding(params, 'height', { min: 0, max: 800, step: 0.1 }).on('change', generatePetals);
+petals.addBinding(params, 'width', { min: 0, max: 800, step: 0.1 }).on('change', () => {
+    generatePetals();
+});
+petals.addBinding(params, 'height', { min: 0, max: 800, step: 0.1 }).on('change', () => {
+    generatePetals();
+});
 petals.addBlade({
     view: 'separator',
 });
 petals.addBinding(params, 'petalX', { min: -1000, max: 1000 }).on('change', generatePetals);
 petals.addBinding(params, 'petalY', { min: -1000, max: 1000 }).on('change', generatePetals);
 
-const path = pane.addFolder({ title: 'Path' });
-path.addBinding(params, 'x1', { min: 0, max: 150, step: 0.1 }).on('change', generatePetals);
-path.addBinding(params, 'x2', { min: 0, max: 150, step: 0.1 }).on('change', generatePetals);
-path.addBinding(params, 'y1', { min: 0, max: 150, step: 0.1 }).on('change', generatePetals);
-path.addBinding(params, 'y2', { min: 0, max: 150, step: 0.1 }).on('change', generatePetals);
-path.addBinding(params, 'z1', { min: 0, max: 150, step: 0.1 }).on('change', generatePetals);
-path.addBinding(params, 'z2', { min: 0, max: 150, step: 0.1 }).on('change', generatePetals);
 
-const shapes = pane.addTab({
-    pages: [
-        { title: 'roundness' },
-        { title: 'Advanced' },
+// Roundness
+const Roundness = pane.addFolder({ title: 'Roundness', expanded: false });
 
-    ],
-});
+Roundness.addBinding(params, 'radius_x1', { min: 0, max: 200 }).on('change', generatePetals);
+Roundness.addBinding(params, 'radius_y1', { min: 0, max: 200 }).on('change', generatePetals);
 
-// SHAPES
-const roundness = shapes.pages[0];
-const applyPreset = {
-    'Round': () => {
-        params.radius_A = 100;
-        params.radius_B = 100;
-        params.radius_C = 100;
-        params.radius_D = 100;
-    },
-    'Square': () => {
-        params.radius_A = 0;
-        params.radius_B = 0;
-        params.radius_C = 0;
-        params.radius_D = 0;
-    },
-    'Alt_1': () => {
-        params.radius_A = 100;
-        params.radius_B = 0;
-        params.radius_C = 100;
-        params.radius_D = 0;
-    },
-    'Alt_2': () => {
-        params.radius_A = 0;
-        params.radius_B = 100;
-        params.radius_C = 0;
-        params.radius_D = 100;
-    }
-};
+Roundness.addBinding(params, 'radius_x2', { min: 0, max: 200 }).on('change', generatePetals);
+Roundness.addBinding(params, 'radius_y2', { min: 0, max: 200 }).on('change', generatePetals);
 
-roundness.addBlade({
-    view: 'list',
-    label: 'Shape',
-    options: [
-        { text: 'Square', value: 'Square' },
-        { text: 'Round', value: 'Round' },
-        { text: 'Alt_1', value: 'Alt_1' },
-        { text: 'Alt_2', value: 'Alt_2' },
-    ],
-    value: 'Square',
-}).on('change', (event) => {
-    const selectedValue = event.value;
-    if (typeof applyPreset[selectedValue] === 'function') {
-        applyPreset[selectedValue]();
-        generatePetals();
-    } else {
-        console.error('Selected preset function does not exist:', selectedValue);
-    }
-});
+Roundness.addBinding(params, 'radius_x3', { min: 0, max: 200 }).on('change', generatePetals);
+Roundness.addBinding(params, 'radius_y3', { min: 0, max: 200 }).on('change', generatePetals);
 
-shapes.pages[1].addBinding(params, 'radius_A', { min: 0, max: 100 }).on('change', generatePetals);
-shapes.pages[1].addBinding(params, 'radius_B', { min: 0, max: 100 }).on('change', generatePetals);
-shapes.pages[1].addBinding(params, 'radius_C', { min: 0, max: 100 }).on('change', generatePetals);
-shapes.pages[1].addBinding(params, 'radius_D', { min: 0, max: 100 }).on('change', generatePetals);
+Roundness.addBinding(params, 'radius_x4', { min: 0, max: 200 }).on('change', generatePetals);
+Roundness.addBinding(params, 'radius_y4', { min: 0, max: 200 }).on('change', generatePetals);
+
+// Custom Shape
+const path = pane.addFolder({ title: 'Custom Shape', expanded: false });
+path.addBinding(params, 'enable', { label: 'Enable' }).on('change', generatePetals);
+path.addBinding(params, 'x1', { min: 0, max: 500, step: 0.1 }).on('change', generatePetals);
+path.addBinding(params, 'y1', { min: 0, max: 500, step: 0.1 }).on('change', generatePetals);
+path.addBinding(params, 'x2', { min: 0, max: 500, step: 0.1 }).on('change', generatePetals);
+path.addBinding(params, 'y2', { min: 0, max: 500, step: 0.1 }).on('change', generatePetals);
+path.addBinding(params, 'x3', { min: 0, max: 500, step: 0.1 }).on('change', generatePetals);
+path.addBinding(params, 'y3', { min: 0, max: 500, step: 0.1 }).on('change', generatePetals);
+path.addBinding(params, 'x4', { min: 0, max: 500, step: 0.1 }).on('change', generatePetals);
+path.addBinding(params, 'y4', { min: 0, max: 500, step: 0.1 }).on('change', generatePetals);
 
 // LAYERS
-const layers = pane.addFolder({ title: 'Layers' });
-layers.addBinding(params, 'layers', { min: 0, max: 500, step: 1 }).on('change', generatePetals);
+const layers = pane.addFolder({ title: 'Layers', expanded: false });
+layers.addBinding(params, 'layers', { min: 0, max: 10, step: 1 }).on('change', generatePetals);
 layers.addBlade({
     view: 'separator',
 });
@@ -100,55 +68,58 @@ layers.addBlade({
 layers.addBinding(params, 'scale', { min: -8, max: 8, step: 0.001 }).on('change', generatePetals);
 
 // ROTATION
-const rotation = pane.addFolder({ title: 'Rotation' });
+const rotation = pane.addFolder({ title: 'Rotation', expanded: false });
 rotation.addBinding(params, 'angle', { min: -180, max: 180, step: 0.1 }).on('change', generatePetals);
 rotation.addBinding(params, 'rotation', { min: -8, max: 8, step: 0.1 }).on('change', generatePetals);
 
 // FILL
-const fill = pane.addFolder({ title: 'Fill' });
+const fill = pane.addFolder({ title: 'Fill', expanded: false });
 
 fill.addBinding(params, 'fill', { label: 'Fill' }).on('change', generatePetals);
 fill.addBlade({ view: 'separator' });
 
-fill.addBinding(params, 'gradientType', {
-    label: 'Type',
-    options: {
-        Linear: 'linear',
-        Radial: 'radial'
-    },
-}).on('change', () => {
-    generatePetals();
-});
+// fill.addBinding(params, 'gradientType', {
+//     label: 'Type',
+//     options: {
+//         Linear: 'linear',
+//         Radial: 'radial'
+//     },
+// }).on('change', () => {
+//     generatePetals();
+// });
 
 fill.addBinding(params, 'position', {
     picker: 'inline',
-    expanded: false,
+    expanded: true,
 }).on('change', () => {
     generatePetals();
 });
 
-fill.addBinding(params, 'gradient_direction', {
-    label: 'Direction',
-    options: {
-        Up: 'up',
-        Down: 'down',
-        Right: 'right',
-        Left: 'left',
-    },
-}).on('change', (value) => { generatePetals(); });
+// fill.addBinding(params, 'gradient_direction', {
+//     label: 'Direction',
+//     options: {
+//         Up: 'up',
+//         Down: 'down',
+//         Right: 'right',
+//         Left: 'left',
+//     },
+// }).on('change', (value) => { generatePetals(); });
 
 fill.addBlade({ view: 'separator' });
-
-fill.addBinding(params, 'color_shift', { min: 0.1, max: 50 }).on('change', generatePetals);
-fill.addBlade({ view: 'separator' });
+// fill.addBinding(params, 'color_shift', { min: 0.1, max: 50 }).on('change', generatePetals);
+// fill.addBlade({ view: 'separator' });
 fill.addBinding(params, 'hue_1', { min: 0, max: 360 }).on('change', generatePetals);
 fill.addBinding(params, 'hue_2', { min: 0, max: 360 }).on('change', generatePetals);
+fill.addBinding(params, 'light_1', { min: 0, max: 100 }).on('change', generatePetals);
+fill.addBinding(params, 'light_2', { min: 0, max: 100 }).on('change', generatePetals);
+fill.addBinding(params, 'sat_1', { min: 0, max: 100 }).on('change', generatePetals);
+fill.addBinding(params, 'sat_2', { min: 0, max: 100 }).on('change', generatePetals);
 fill.addBlade({ view: 'separator' });
 
 fill.addBinding(params, 'opacity_1', { min: 0, max: 1, step: 0.01 }).on('change', generatePetals);
 fill.addBinding(params, 'opacity_2', { min: 0, max: 1, step: 0.01 }).on('change', generatePetals);
 
-const border = pane.addFolder({ title: 'Border' });
+const border = pane.addFolder({ title: 'Border', expanded: false });
 border.addBinding(params, 'strokeWidth', { min: 0, max: 50 }).on('change', generatePetals);
 border.addBinding(params, 'strokeStyle', {
     label: 'Style',
@@ -192,9 +163,19 @@ border.addBinding(params, 'strokeStyle', {
 //     document.documentElement.style.setProperty('--pattern-color-2', value);
 // });
 
-const effect = pane.addFolder({ title: 'Effect' });
+const effect = pane.addFolder({ title: 'Effect', expanded: false });
 effect.addBinding(params, 'blur', { min: 0, max: 2, step: 0.01 }).on('change', generatePetals);
 effect.addBinding(params, 'shadow', { min: 0, max: 50, step: 0.01 }).on('change', generatePetals);
+
+// ENTROPY
+const evolution = pane.addFolder({ title: 'Evolution', expanded: true });
+evolution.addBinding(params, 'entropy', {
+    view: 'cameraring',
+    series: 0,
+}).on('change', () => {
+    generatePetals();
+});
+
 
 // // Manage
 // const state = pane.exportState();
