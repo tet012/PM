ba49408ee2ad7ff13c7917e8dc10b50b6bde8f4f
tet012 <120034406+tet012@users.noreply.github.com>
2024-03-26 23:06:27 +0100

    added shadows, gradients & lights

--- flower.js
+++ flower.js
@@ -1,8 +1,9 @@
 let perlin = new Perlin();
 
 class Flower {
-    constructor(x, y, p, h_factor, smooth_factor, flowField, resolution, index, totalFlowers, patternType, flower_color, core_color, depth = 0, maxDepth = 2, parentAngle = null) {
-        Object.assign(this, { x, y, p, h_factor, smooth_factor, flowField, resolution, index, totalFlowers, patternType, flower_color, core_color, depth, maxDepth, parentAngle });
+    constructor(x, y, p, h_factor, smooth_factor, flowField, resolution, index, totalFlowers, patternType, fc_1, fc_2, core_color, depth = 0, maxDepth = 2, parentAngle = null, hasFlower = true) {
+        Object.assign(this, { x, y, p, h_factor, smooth_factor, flowField, resolution, index, totalFlowers, patternType, fc_1, fc_2, core_color, depth, maxDepth, parentAngle, hasFlower });
+
         this.coreRadius = 20;
         this.branches = [];
         this.points = this.calculatePoints();
@@ -13,7 +14,7 @@ class Flower {
         let [lastX, lastY] = [this.x, this.y];
         for (let i = 0; i < this.p; i++) {
             const { nextX, nextY } = this.getNextPoint(lastX, lastY, i);
-            if (nextY < canvas.height * 0.1) break;
+            if (nextY < canvas.height * R.random_num(0.1, 0.3)) break;
             points.push([nextX, nextY]);
             [lastX, lastY] = [nextX, nextY];
             this.attemptBranching(lastX, lastY, i, points);
@@ -23,12 +24,11 @@ class Flower {
 
     getNextPoint(x, y, index) {
         const { h_factor } = this;
-        const angle = Math.random() * 2 * Math.PI;
-        const h_variation = (Math.random() - 0.5) * h_factor;
+        const angle = R.random_num(0, 1) * 2 * Math.PI;
+        const h_variation = (R.random_num(0, 1) - 0.5) * h_factor;
         let nextX = x + Math.cos(angle) * 10 + h_variation;
         let nextY = this.calculateNextY(y, index, angle);
 
-        // Check if the next point is within the canvas boundaries
         nextX = Math.max(0, Math.min(nextX, canvas.width));
         nextY = Math.max(0, Math.min(nextY, canvas.height));
 
@@ -54,24 +54,27 @@ class Flower {
             default:
                 yOffset = 0;
         }
-        const upwardOffset = 30;
+        const upwardOffset = R.random_int(10, 60);
         return y - upwardOffset + yOffset;
     }
 
     attemptBranching(x, y, index, points) {
         const { depth, maxDepth, p, h_factor, smooth_factor, flowField, resolution, index: flowerIndex, totalFlowers, patternType, flower_color, core_color } = this;
-        if (Math.random() < 0.05 && depth < maxDepth) {
+        if (R.random_bool(0.5) && depth < maxDepth) {
             const angle = points.length > 1 && index > 0 ? Math.atan2(y - points[index - 1][1], x - points[index - 1][0]) : 0;
-            const branchAngle = angle + (Math.random() * 0.8 - 0.4);
-            const branchLength = Math.floor(Math.random() * (p / 2)) + 5;
+            const branchAngle = angle + (R.random_num(0.4, 0.8));
+            const branchLength = R.random_int(10, p / 2);
             if (flowField && flowField.length > 0) {
                 const [prevX, prevY] = points[index - 1] || [x, y];
+                const fc_1 = R_Col(SP);
+                const fc_2 = R_Col(SP);
                 const dx = x - prevX;
                 const dy = y - prevY;
-                const dist = Math.sqrt(dx * dx + dy * dy);
                 const branchStartX = prevX + dx * 0.9;
                 const branchStartY = prevY + dy * 0.9;
-                this.branches.push(new Flower(branchStartX, branchStartY, branchLength, h_factor, smooth_factor, flowField, resolution, flowerIndex, totalFlowers, patternType, flower_color, core_color, depth + 1, maxDepth, branchAngle));
+                const hasFlower = R.random_bool(0.5);
+                const branch = new Flower(branchStartX, branchStartY, branchLength, h_factor, smooth_factor, flowField, resolution, flowerIndex, totalFlowers, patternType, fc_1, fc_2, core_color, depth + 1, maxDepth, branchAngle, hasFlower);
+                this.branches.push(branch);
             }
         }
     }
@@ -128,7 +131,7 @@ class Flower {
     }
 
     drawPetals() {
-        const numPetals = Math.floor(Math.random() * 8 + 4);
+        const numPetals = R.random_int(4, 16);
         for (let i = 0; i < numPetals; i++) {
             this.drawPetal(i, numPetals);
         }
@@ -136,61 +139,96 @@ class Flower {
     }
 
     drawPetal(index, total) {
-        const { coreRadius, points, flower_color, depth } = this;
+        const { fc_1, fc_2, coreRadius, points, depth } = this;
         const angle = (index / total) * 2 * Math.PI;
-        const scaleFactor = Math.pow(0.7, depth);
-        const petalLength = coreRadius * scaleFactor * (0.8 + Math.random() * 0.4);
-        const petalWidth = coreRadius / 2 * scaleFactor * (0.8 + Math.random() * 0.4);
+        const scaleFactor = Math.pow(0.8, depth);
+        const petalLength = coreRadius * scaleFactor * (0.8 + R.random_num(0.4, 1.2));
+        const petalWidth = coreRadius / 2 * scaleFactor * (0.8 + R.random_num(0.4, 1.2));
         const [petalX, petalY] = [points[points.length - 1][0] + Math.cos(angle) * coreRadius * 1.5, points[points.length - 1][1] + Math.sin(angle) * coreRadius * 1.5];
         const noiseValue = perlin.noise(petalX * 0.05, petalY * 0.05);
         const n = Math.abs((noiseValue - 0.5) * 2) * scaleFactor;
+
         ctx.save();
+        ctx.translate(petalX + n, petalY + n);
+        ctx.rotate(angle);
+
         ctx.beginPath();
-        ctx.ellipse(petalX + n, petalY + n, petalLength, petalWidth, angle, 0, 2 * Math.PI);
-        ctx.fillStyle = flower_color;
+
+        if (mode === 1) {
+            ctx.ellipse(0, 0, petalLength, petalWidth, 0, 0, 2 * Math.PI);
+        } else if (mode === 2) {
+            ctx.rect(-petalLength, -petalWidth / 2, petalLength * 2, petalWidth);
+        } else {
+            const numSides = R.random_int(3, 6);
+            const angleStep = (Math.PI * 2) / numSides;
+            ctx.moveTo(petalLength, 0);
+            for (let i = 1; i < numSides; i++) {
+                const x = Math.cos(i * angleStep) * petalLength;
+                const y = Math.sin(i * angleStep) * petalWidth;
+                ctx.lineTo(x, y);
+            }
+            ctx.closePath();
+        }
+
+        const flower_gradient = ctx.createLinearGradient(-petalLength, 0, petalLength, 0);
+        flower_gradient.addColorStop(0, fc_1);
+        flower_gradient.addColorStop(1, fc_2);
+        ctx.fillStyle = flower_gradient;
         ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
         ctx.shadowBlur = petalWidth * 0.4;
         ctx.shadowOffsetY = petalWidth * 0.2;
         ctx.fill();
         ctx.restore();
+
         this.drawPetalStroke(petalX + n, petalY + n, petalLength, petalWidth, angle, scaleFactor);
     }
 
     drawPetalStroke(x, y, length, width, angle, scaleFactor) {
-        const { coreRadius, points, flower_color, depth } = this;
-        const petalLength = coreRadius * scaleFactor * (0.8 + Math.random() * 0.4);
-        const petalWidth = coreRadius / 2 * scaleFactor * (0.8 + Math.random() * 0.4);
+        const { coreRadius, points, flower_color } = this;
+        const petalLength = coreRadius * scaleFactor * (0.8 + R.random_num(0.4, 1.2));
+        const petalWidth = coreRadius / 2 * scaleFactor * (0.8 + R.random_num(0.4, 1.2));
         const [petalX, petalY] = [points[points.length - 1][0] + Math.cos(angle) * coreRadius * 1.5, points[points.length - 1][1] + Math.sin(angle) * coreRadius * 1.5];
         const noiseValue = perlin.noise(petalX * 0.05, petalY * 0.05);
         const n = Math.abs((noiseValue - 0.5) * 2) * scaleFactor;
+
+        ctx.save();
+        ctx.translate(petalX + n, petalY + n);
+        ctx.rotate(angle);
+
+
         ctx.beginPath();
-        ctx.ellipse(petalX + n, petalY + n, petalLength, petalWidth, angle, 0, 2 * Math.PI);
+        if (mode === 1) {
+            ctx.ellipse(0, 0, petalLength, petalWidth, 0, 0, 2 * Math.PI);
+        } else if (mode === 2) {
+            ctx.rect(-petalLength, -petalWidth / 2, petalLength * 2, petalWidth);
+        } else {
+            const numSides = R.random_int(3, 6);
+            const angleStep = (Math.PI * 2) / numSides;
+            ctx.moveTo(petalLength, 0);
+            for (let i = 1; i < numSides; i++) {
+                const x = Math.cos(i * angleStep) * petalLength;
+                const y = Math.sin(i * angleStep) * petalWidth;
+                ctx.lineTo(x, y);
+            }
+            ctx.closePath();
+        }
+        ctx.restore();
+
         ctx.fillStyle = flower_color;
         ctx.strokeStyle = 'black';
         ctx.lineWidth = scaleFactor;
         ctx.stroke();
     }
 
-    drawShadowEllipse() {
-        const { coreRadius, points } = this;
-        const [centerX, centerY] = points[points.length - 1];
-        const shadowEllipseRadius = coreRadius * 1.5;
-        const shadowOffsetY = coreRadius * 0.5;
-        const shadowBlur = coreRadius * 0.7;
-        ctx.save();
-        ctx.beginPath();
-        ctx.ellipse(centerX, centerY - shadowOffsetY, shadowEllipseRadius, shadowEllipseRadius * 0.6, 0, 0, 2 * Math.PI);
-        ctx.shadowColor = 'rgba(0, 0, 0, 1)';
-        ctx.shadowBlur = shadowBlur;
-        ctx.shadowOffsetY = shadowOffsetY;
-        ctx.fill();
-        ctx.restore();
-    }
-
     draw() {
         this.drawStem();
-        this.branches.forEach(branch => branch.draw());
-        this.drawPetals();
-        this.drawCore();
+        this.branches.forEach(branch => {
+            branch.draw();
+        });
+        if (this.hasFlower) {
+            this.drawPetals();
+            this.drawCore();
+        }
     }
 }
+
--- script.js
+++ script.js
@@ -13,21 +13,30 @@ const middleY = canvas.height / 2;
 
 const selectedPalette = R.random_int(0, 10);
 const SP = pals[selectedPalette].map(val => `hsl(${val})`);
-const color_1 = R_Col(SP)
-const color_2 = R_Col(SP)
-let h_factor
 
+let mode = R.random_int(0, 2);
+let numSides = R.random_int(3, 6);
+
+let h_factor
+let wOff = R.random_num(0.1, 0.4)
 let startY = H * 0.9;
 
 function animate() {
+    if (R.random_bool(0.5)) {
+        color_1 = R_Col(SP)
+        color_2 = R_Col(SP)
+    } else {
+        color_1 = color_2 = R_Col(SP)
+    }
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
     gradient.addColorStop(0, color_1);
-    gradient.addColorStop(1, color_1);
+    gradient.addColorStop(1, color_2);
     ctx.fillStyle = gradient;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    const smooth_factor = 4;
+    const smooth_factor = R.random_int(0, 4);
 
     const resolution = 30;
     const noiseScale = 0.0001;
@@ -36,23 +45,59 @@ function animate() {
 
     const flowField = flowfield(W, H, resolution, noiseScale, noiseStrength, angleOffset);
 
-    const totalFlowers = Math.random() * 3 + 1;
-    h_factor = Math.floor(Math.random() * 50) + 1;
+    const totalFlowers = R.random_num(1, 50);
+    // const totalFlowers = Math.random() * 50 + 1;
+    h_factor = R.random_num(1, 50)
+
+    const sunX = W * R.random_num(0, 1);
+    const sunY = H * R.random_num(0, .7);
+    const sunRadius = R.random_num(125, 250)
+    drawSun(sunX, sunY, sunRadius);
+
 
     const angleType = R.random_choice(['straight', 'curvy', 'random']);
     for (let f = 0; f < totalFlowers; f++) {
-        const [flower_color, core_color] = [R_Col(SP), R_Col(SP)];
-        const p = Math.floor(Math.random() * 10 + 5);
-        const startX = W / 3 + Math.random() * (W / 3);
-        const flower = new Flower(startX, startY, p, h_factor, smooth_factor, flowField, resolution, f, totalFlowers, angleType, flower_color, core_color);
+        const fc_1 = R_Col(SP);
+        const fc_2 = R_Col(SP);
+        const core_color = R_Col(SP);
+        const p = Math.floor(R.random_num(5, 10));
+        startX = W / 3 + R.random_num(0, 1) * (W / 3);
+        const flower = new Flower(startX, startY, p, h_factor, smooth_factor, flowField, resolution, f, totalFlowers, angleType, fc_1, fc_2, core_color);
         flower.draw();
     }
-    ctx.beginPath();
-    ctx.moveTo(W * 0.3, H * 0.9);
-    ctx.lineTo(W * 0.7, H * 0.9);
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = 2;
-    ctx.stroke();
+
+    drawGround();
 }
+
+function drawGround() {
+    const xOff = W * 0.3;
+    ctx.save();
+    for (let i = xOff; i < W - xOff; i += 1) {
+        const p_size = 2
+        ctx.fillStyle = 'black';
+        ctx.fillRect(i, H * 0.9 - p_size / 2, p_size, p_size);
+    }
+    ctx.restore();
+}
+
+function drawSun(x, y, radius) {
+    ctx.save();
+
+    ctx.shadowColor = 'rgba(255, 255, 0, 0.5)';
+    ctx.shadowBlur = 160;
+    ctx.shadowOffsetX = 0;
+    ctx.shadowOffsetY = 0;
+
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    const gradient = ctx.createRadialGradient(x, y, 0, x, y, radius);
+    gradient.addColorStop(0, 'rgba(255, 255, 0, 0.9)');
+    gradient.addColorStop(1, 'rgba(255, 255, 0, 0.6)');
+    ctx.fillStyle = gradient;
+    ctx.fill();
+
+    ctx.restore();
+}
+
 
 animate();
